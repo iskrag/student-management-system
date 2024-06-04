f236e1095440668f2e3636c6108c480dd3547b78
iskra <iskrag@gmail.com>
2024-06-04 17:13:54 +0200

    Create table and populate with data from SQL database

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from PyQt6.QtWidgets import (QApplication, QLabel, QWidget, QGridLayout,
-                             QLineEdit, QPushButton, QComboBox, QMainWindow)
+                             QLineEdit, QPushButton, QComboBox, QMainWindow,
+                             QTableWidget, QTableWidgetItem)
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Student Management System')
-        # grid = QGridLayout()
 
         file_menu_item = self.menuBar().addMenu('&File')
         help_menu_item = self.menuBar().addMenu('&Help')
@@ -19,8 +20,26 @@ class MainWindow(QMainWindow):
         about_action = QAction('About', self)
         help_menu_item.addAction(about_action)
 
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(('Id', 'Name', 'Course', 'Mobile'))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect('database.db')
+        result = connection.execute('SELECT * FROM students')
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+        connection.close()
+
 
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
